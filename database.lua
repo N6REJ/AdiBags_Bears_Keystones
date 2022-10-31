1053428e4a8d0086f3fd977edc6e1c44ad2a6fa5
Bear <programming@hallhome.us>
2022-10-31 14:41:39 -0500

    Initial release for 10.0.0

--- database.lua
+++ database.lua
@@ -1,4 +1,4 @@
--- AdiBags_Queens_Conservatory - Database
+-- AdiBags_Bears_Keystones - Database
 -- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
 -- Source on GitHub: https://n6rej.github.io
 ---@type
@@ -8,47 +8,24 @@ local addonName, addonTable = ...;
 -- Get locals namse
 local L = addonTable.locales;
 local Expansion = L["Shadowlands"]
+
 -- Create addon table
 local db = { }
 
-db.name = "Queens Conservatory"
-db.desc = "Seeds & Catalyst Plots used by the Night Fae in the Queens Conservatory"
+db.name = "Bears Keystones"
+db.desc = "Sorts Keystones into their own category"
 
 -- Filter info
 db.Filters = {
-	["Seeds"] = {
-		uiName = "Queens Conservatory Seeds",
-		uiDesc = "Seeds used by Night Fae in the Queens Conservatory",
-		title = "Seeds",
+	["Keystones"] = {
+		uiName = "Bears Mythic+ Keystone",
+		uiDesc = "Put Mythic+ keystones in their own category.",
+		title = "Keystones",
 		items = {
 			-- ID,	--Item name
-			[178881] = true,	-- Dutiful Spirit
-			[178880] = true,	-- Greater Dutiful Spirit
-			[178879] = true,	-- Divine Dutiful Spirit
-			[178874] = true,	-- Martial Spirit
-			[178877] = true, 	-- Greater Martial Spirit
-			[178878] = true, 	-- Divine Martial Spirit
-			[177698] = true, 	-- Untamed Spirit
-			[177699] = true, 	-- Greater Untamed Spirit
-			[177700] = true, 	-- Divine Untamed Spirit
-			[178882] = true, 	-- Prideful Spirit
-			[178883] = true, 	-- Greater Prideful Spirit
-			[178884] = true,	-- Divine Prideful Spirit
-			[183805] = true		-- Tranquil Spirit of the Cosmos
+			[180653] = true,	-- Shadowlands Mythic+ Keystone
 		},
-	},
-	["Plots"] = {
-		uiName = "Queens Conservatory Catalyst",
-		uiDesc = "Catalyst Plots used by Night Fae in the Queens Conservatory",
-		title = "Plots",
-		items = {
-			-- ID,	--Item name
-		   [176832] = true,	-- Wildseed root
-		   [176922] = true, -- Wild Nightbloom
-		   [176921] = true,	-- Temporal Leaves
-		   [183522] = true	-- Wildseed Root Grain Seeds
-	   },
-	},
+	}
 }
 
 -- now that db is populated lets pass it on.
